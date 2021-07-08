d48e3fa0969054b1d6b35b06b4f49f36e6db9955
MasakoIdei <tarte.groseilles@gmail.com>
2021-07-08 09:13:39 +0000

    [add]各コントローラーの追加、top画面とnavberの作成

---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,21 @@
+class Public::CartItemsController < ApplicationController
+
+  def index
+  end
+
+  def destroy
+  end
+
+　#カートを空にする
+  def destroy_all
+  end
+
+　#商品の追加
+  def create
+  end
+
+  #商品内容の更新
+  def update
+  end
+
+end
---
+++ app/controllers/public/items_controller.rb
@@ -0,0 +1,11 @@
+class Public::ItemsController < ApplicationController
+
+　#商品一覧の表示
+  def index
+  end
+
+  #商品詳細表示
+  def show
+  end
+
+end
