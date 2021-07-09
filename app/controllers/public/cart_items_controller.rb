cd515f6ded29e40a377e038f42a0d406845c497d
MasakoIdei <tarte.groseilles@gmail.com>
2021-07-09 11:14:25 +0000

    [add]カート実装に伴うアソシエーション、商品情報のカートへの受け渡し

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,21 +1,28 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    # @cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
   end
 
   def destroy
   end
 
-　#カートを空にする
+ #カートを空にする
   def destroy_all
   end
 
-　#商品の追加
+ #商品の追加
   def create
   end
 
   #商品内容の更新
   def update
   end
+
+  private
+
+   def cart_items_params
+    params.require(:cart_item).permit(:quantity, :item_id)
+   end
 
 end
