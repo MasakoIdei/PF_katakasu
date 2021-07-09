f3c9649d454ec8c2ce5f61238bea790cd60e5620
MasakoIdei <tarte.groseilles@gmail.com>
2021-07-09 08:22:49 +0000

    [add]カートモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_092830) do
+ActiveRecord::Schema.define(version: 2021_07_09_081145) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2021_07_08_092830) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "item_id"], name: "index_cart_items_on_customer_id_and_item_id", unique: true
   end
 
   create_table "customers", force: :cascade do |t|
