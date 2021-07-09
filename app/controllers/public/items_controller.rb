8d7b63707c3c2c5e22ee7981254495b4c5f48b08
MasakoIdei <tarte.groseilles@gmail.com>
2021-07-09 03:50:05 +0000

    [add]背景カラーの指定

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,13 @@
 class Public::ItemsController < ApplicationController
 
-　#商品一覧の表示
+  #商品一覧の表示
   def index
+    @items = Item.all
   end
 
   #商品詳細表示
   def show
+    @item = Item.find(params[:id])
   end
 
 end
