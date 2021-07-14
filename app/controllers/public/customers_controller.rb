445cff86c0dcb1d9e2e0fe181f17b8863dc003e4
MasakoIdei <tarte.groseilles@gmail.com>
2021-07-14 16:52:36 +0000

    [add]会員情報編集画面作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
 
 #会員マイページ
   def show
@@ -7,10 +8,23 @@ class Public::CustomersController < ApplicationController
 
 #会員情報の編集
   def edit
+    @customer = Customer.find(params[:id])
   end
 
  #編集内容の更新
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to my_page_path, notice: '会員情報の更新が完了しました。'
+    else
+      render "edit"
+    end
+
   end
+
+  private
+   def customer_params
+      params.require(:customer).permit(:name, :email, :postal_code, :address, :telephone_number)
+   end
 
 end
